36e41a5001708cfde8236f713018a061fee4bb46
diemkhang1912@gmail.com <diemkhang1912@gmail.com>
2021-05-30 21:24:11 +0700

    Practice

--- src/vn/plusplus/pratice/pacticels4.java
+++ src/vn/plusplus/pratice/pacticels4.java
@@ -3,21 +3,25 @@ package vn.plusplus.pratice;
 public class pacticels4 {
     //tăng mỗi phần tử trong mảng lên 2 đơn vị.
     public static void main(String[] args) {
-        int myArray[] = {24,127,20,2,24,215};
-        System.out.println("BEFORE call increase function: "+ toString(myArray));
-        increase(myArray,2);
-        System.out.println("AFTER call increase function: " + toString(myArray));
+        String s = new String(new char[] { 'H', 'e', 'l', 'l', 'o' });
+        String c = "Hello PlusPlus Academy!";
+        String a = "Hello".concat("PlusPlus Academy");
+
+//        int myArray[] = {24,127,20,2,24,215};
+//        System.out.println("BEFORE call increase function: "+ toString(myArray));
+//        increase(myArray,2);
+//        System.out.println("AFTER call increase function: " + toString(myArray));
     }
-    public static String toString(int[] myArray){
-        String result = "";
-        for(int i=0;i < myArray.length;i++){
-            result += result.equals("") ? myArray[i] : ", " + myArray[i];
-        }
-        return result;
-    }
-    public static void increase(int[] myArray, int step ){
-        for(int i=0;i<myArray.length;i++){
-            myArray[i] += step;
-        }
-    }
+//    public static String toString(int[] myArray){
+//        String result = "";
+//        for(int i=0;i < myArray.length;i++){
+//            result += result.equals("") ? myArray[i] : ", " + myArray[i];
+//        }
+//        return result;
+//    }
+//    public static void increase(int[] myArray, int step ){
+//        for(int i=0;i<myArray.length;i++){
+//            myArray[i] += step;
+//        }
+//    }
 }
