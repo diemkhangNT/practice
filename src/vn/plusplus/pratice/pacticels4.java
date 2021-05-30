bc379d7b99fa73a363d445fed831e5b873685726
diemkhang1912@gmail.com <diemkhang1912@gmail.com>
2021-05-30 19:48:55 +0700

    Practice

---
+++ src/vn/plusplus/pratice/pacticels4.java
@@ -0,0 +1,23 @@
+package vn.plusplus.pratice;
+
+public class pacticels4 {
+    //tăng mỗi phần tử trong mảng lên 2 đơn vị.
+    public static void main(String[] args) {
+        int myArray[] = {24,127,20,2,24,215};
+        System.out.println("BEFORE call increase function: "+ toString(myArray));
+        increase(myArray,2);
+        System.out.println("AFTER call increase function: " + toString(myArray));
+    }
+    public static String toString(int[] myArray){
+        String result = "";
+        for(int i=0;i < myArray.length;i++){
+            result += result.equals("") ? myArray[i] : ", " + myArray[i];
+        }
+        return result;
+    }
+    public static void increase(int[] myArray, int step ){
+        for(int i=0;i<myArray.length;i++){
+            myArray[i] += step;
+        }
+    }
+}
